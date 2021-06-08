a2799c1dd52aa1737b106796c431e7fd27acf8f6
Yafei-Guo-coder <yafeiguo116@gmail.com>
2021-06-08 09:50:28 +0800

    add function to calVcfAverageDepth

--- Vmap3/src/FunctionV/CountSite.java
+++ Vmap3/src/FunctionV/CountSite.java
@@ -1,5 +1,7 @@
 package FunctionV;
 
+import gnu.trove.list.array.TDoubleArrayList;
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 import pgl.infra.utils.IOUtils;
 import pgl.infra.utils.PStringUtils;
 
@@ -7,6 +9,8 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class CountSite {
@@ -113,6 +117,9 @@ public class CountSite {
             e.printStackTrace();
         }
     }
+    /**
+     * 将raw vcf文件中的snp和indels分开输出到两个文件中。
+     */
     public static void splitVcf(String infileDirS) {
         List<File> fsList = AoFile.getFileListInDir(infileDirS);
         for (File f : fsList){
@@ -164,5 +171,88 @@ public class CountSite {
                 e.printStackTrace();
             }
         }
+    }
+    /**
+     * 结果只有一列 depth平均深度
+     *
+     * @param infileDirS
+     * @param outfileDirS
+     */
+    public void calVcfAverageDepth(String infileDirS, String outfileDirS) {
+        File[] fs = new File(infileDirS).listFiles();
+        for (int i = 0; i < fs.length; i++) {
+            if (fs[i].isHidden()) {
+                fs[i].delete();
+            }
+        }
+        fs = new File(infileDirS).listFiles();
+        Arrays.sort(fs);
+        List<File> fsList = Arrays.asList(fs);
+        fsList.parallelStream().forEach(f -> {
+            String infileS = f.getAbsolutePath();
+            String outfileS = null;
+            BufferedReader br = null;
+            if (infileS.endsWith(".vcf")) {
+                br = IOUtils.getTextReader(infileS);
+                outfileS = new File(outfileDirS, f.getName().replaceFirst(".vcf", ".depth.txt.gz")).getAbsolutePath();
+            } else if (infileS.endsWith(".vcf.gz")) {
+                br = IOUtils.getTextGzipReader(infileS);
+                outfileS = new File(outfileDirS, f.getName().replaceFirst(".vcf.gz", ".depth.txt.gz")).getAbsolutePath();
+            }
+            BufferedWriter bw = IOUtils.getTextGzipWriter(outfileS);
+            String temp = null;
+            int cnt = 0;
+            try {
+                while ((temp = br.readLine()).startsWith("##")) {
+                }
+                List<String> linetaxa = PStringUtils.fastSplit(temp, "\t");
+                bw.write("AverageDepth");
+                bw.newLine();
+                String[] taxa = new String[linetaxa.size() - 9];
+                cnt = 0;
+                while ((temp = br.readLine()) != null) {
+                    cnt++; // 对snp开始计数
+                    if (cnt % 1000000 == 0) {
+                        System.out.println(String.valueOf(cnt) + " lines");
+                    }
+
+                    TDoubleArrayList depthList = new TDoubleArrayList();
+                    TDoubleArrayList PValueList = new TDoubleArrayList();
+                    List<String> l = new ArrayList<>();
+                    l = PStringUtils.fastSplit(temp, "\t");
+                    String chr = l.get(0);
+                    String pos = l.get(1);
+                    for (int i = 0; i < taxa.length; i++) {
+                        String genoS = l.get(i + 9);
+                        if (genoS.startsWith(".")) {
+                            depthList.add(0);
+                            continue;
+                        }
+                        List<String> ll = PStringUtils.fastSplit(genoS, ":");
+                        List<String> lll = PStringUtils.fastSplit(ll.get(1), ",");
+                        int depth = Integer.valueOf(lll.get(0)) + Integer.valueOf(lll.get(1));
+                        depthList.add(depth);
+                    }
+                    double[] dep = depthList.toArray();
+                    DescriptiveStatistics d = new DescriptiveStatistics(dep);
+                    double relativeMean = d.getMean();
+                    double sd = d.getStandardDeviation();
+                    StringBuilder sb = new StringBuilder();
+                    sb.append(String.format("%.4f", relativeMean));
+                    bw.write(sb.toString());
+                    bw.newLine();
+                }
+                br.close();
+                bw.flush();
+                bw.close();
+                System.out.println();
+                System.out.println(f.getName() + "\t" + cnt + " sites is completed");
+            } catch (Exception e) {
+                System.out.println(temp);
+                e.printStackTrace();
+                System.exit(1);
+            }
+
+        });
     }
 }
